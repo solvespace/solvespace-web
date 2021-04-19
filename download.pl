7262816966cad60ca37d2a3381edd82fb540df1f
app4soft <appsoft@ua.fm>
2021-04-19 03:10:55 +0300

    Update download.pl

--- download.pl
+++ download.pl
@@ -53,9 +53,10 @@ to Applications to install. Use 3.0, since it is vast improvement over previous 
 packaging status on <a rel="nofollow" href="https://repology.org/project/solvespace/versions">Repology</a>
 or <a rel="nofollow" href="https://pkgs.org/search/?q=solvespace">Packages Search</a>), or you can obtain
 it from the <a rel="nofollow" href="https://snapcraft.io/solvespace">Snap Store</a> and 3rd-party
-<a rel="nofollow" href="https://notesalexp.org/">PPA for Debian and Ubuntu</a>. You may also
-<a rel="nofollow" href="https://github.com/solvespace/solvespace/tree/2.x#building-on-linux">build</a>
-SolveSpace from source and install it youself.
+<a rel="nofollow" href="https://notesalexp.org/">PPA for Debian and Ubuntu</a>.
+
+You may also <a rel="nofollow" href="https://github.com/solvespace/solvespace#via-source-code">build</a>
+SolveSpace <b>from source</b> and install it yourself.
 </p>
 
 <p>Files saved by 3.0 may be readable by earlier versions, but some
